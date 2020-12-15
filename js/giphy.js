5c340d9d95245673c15ce8173ac7ee007e74433b
aditee-singh <singhaditee33@gmail.com>
2020-12-15 22:05:40 +0530

    Added giphy project

---
+++ js/giphy.js
@@ -0,0 +1,31 @@
+
+            function searchFunction(){
+                var giphy_name=document.getElementById("searchitem").value;
+                var url="https://api.giphy.com/v1/gifs/search?api_key=EIkbwVRNCK8YVEfOVdMHG3ujfSCFjGVO&q="+giphy_name+"&limit=1";
+                console.log(url);
+                fetch(url)
+                .then(response=>response.json())
+                .then(content=>{
+                    console.log(content.data)
+                    document.getElementById("gif").src=content.data[0].images.fixed_height_still.url;
+                    document.getElementById("gif").props=content.data[0].images.downsized.url;
+                    document.getElementById('gif').style.display='block';
+                })
+                .catch((err)=>console.log(err));
+            }
+            function playPause(){
+                var temp=document.getElementById("gif").src;
+                document.getElementById("gif").src=document.getElementById("gif").props;
+                document.getElementById("gif").props=temp;
+            }
+            function randomFunction() {
+                var url="https://api.giphy.com/v1/gifs/random?&api_key=dc6zaTOxFJmzC&limit=1";    
+                fetch(url)
+                .then(response=>response.json())
+                .then(content=>{
+                    console.log(content.data);
+                    document.getElementById("gif").src = content.data.images.fixed_height_still.url;
+	                document.getElementById("gif").props = content.data.images.downsized.url;
+  })
+  .catch((err) => alert(err));
+}
