35e8bfc48f5637b0904efb9aecf3ff9bc73fba8b
aditee-singh <singhaditee33@gmail.com>
2020-12-17 01:18:23 +0530

    Added loader and hover Pause button

--- js/giphy.js
+++ js/giphy.js
@@ -1,17 +1,30 @@
 
             function searchFunction(){
+                document.getElementById("searchButton").disabled = true;
+                document.getElementById("randomButton").disabled = true;
+                document.getElementById("gif").src="img/loading.webp";
+                document.getElementById('gif').style.display='block';
                 var giphy_name=document.getElementById("searchitem").value;
                 var url="https://api.giphy.com/v1/gifs/search?api_key=EIkbwVRNCK8YVEfOVdMHG3ujfSCFjGVO&q="+giphy_name+"&limit=1";
                 console.log(url);
                 fetch(url)
                 .then(response=>response.json())
-                .then(content=>{
+                .then(content=>{     
                     console.log(content.data)
                     document.getElementById("gif").src=content.data[0].images.downsized.url;
                     document.getElementById("gif").props=content.data[0].images.fixed_height_still.url;
+                    document.getElementById("searchButton").disabled = false;
+                    document.getElementById("randomButton").disabled = false;
                     document.getElementById('gif').style.display='block';
+                    document.getElementById('searchitem').value="";
                 })
                 .catch((err)=>console.log(err));
+                }
+            function showPause(){
+                document.getElementById('playpause').style.display='block';  
+            }
+            function hidePause(){
+                document.getElementById('playpause').style.display='none';
             }
             function playPause(){
                 var temp=document.getElementById("gif").src;
@@ -19,6 +32,8 @@
                 document.getElementById("gif").props=temp;
             }
             function randomFunction() {
+                document.getElementById("gif").src="img/loading.webp";
+                document.getElementById('gif').style.display='block';
                 var url="https://api.giphy.com/v1/gifs/random?&api_key=EIkbwVRNCK8YVEfOVdMHG3ujfSCFjGVO&limit=1";    
                 fetch(url)
                 .then(response=>response.json())
@@ -27,6 +42,6 @@
                     document.getElementById("gif").src = content.data.images.downsized.url;
                     document.getElementById("gif").props =content.data.images.fixed_height_still.url;
                     document.getElementById('gif').style.display='block'; 
-  })
+                })
   .catch((err) => alert(err));
-}
+} 
